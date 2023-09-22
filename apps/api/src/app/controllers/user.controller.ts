4fd82354cd00af3af2f79f49e0b87f85c5ccd012
Mirgo25 <mirgo2513@gmail.com>
2023-09-22 18:26:25 +0300

    added API app to project; realized authorization via JWT

---
+++ apps/api/src/app/controllers/user.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JWTAuthGuard } from '../guards/jwt.guard';
+import { UserId } from '../decorators/user.decorator';
+
+@Controller('user')
+export class UserController {
+  constructor() { }
+
+  @UseGuards(JWTAuthGuard)
+  @Get('info')
+  async register(@UserId() userId: string) { }
+}
